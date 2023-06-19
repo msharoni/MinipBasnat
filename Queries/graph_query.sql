5c75ff3472316d176ac1d224133652c5b8872a8b
msharoni <92259206+msharoni@users.noreply.github.com>
2023-06-19 13:39:40 +0300

    Update graph_query.sql

--- Queries/graph_query.sql
+++ Queries/graph_query.sql
@@ -1,9 +1,10 @@
-SELECT id, firstname, lastname, cost,
+SELECT id, firstname, lastname, cost, gender,
   NTILE(10) OVER (ORDER BY percent) AS group_number
 FROM (
-  SELECT id, firstname, lastname, cost,
+  SELECT id, firstname, lastname, cost, gender,
     (cost / (SELECT AVG(cost) FROM bed B WHERE B.zip = Bed.zip)) * 100 AS percent
   FROM ((Human NATURAL JOIN Student) INNER JOIN Rent ON Id = Studentid) NATURAL JOIN Bed
-  WHERE cost > (SELECT AVG(cost) FROM bed B WHERE B.zip = Bed.zip)
+  WHERE cost BETWEEN 100 AND 140
 ) subquery
-WHERE percent BETWEEN 100 AND 140;
+WHERE cost > (SELECT AVG(cost) FROM bed B WHERE B.zip = Bed.zip)
+GROUP BY id, firstname, lastname, cost, gender, percent;
